6f721a1f822ae2b8012496ab6072513cdc78c4c8
mrinmaydeka036-oss <mrinmaydeka036@gmail.com>
2026-07-16 10:41:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// js
+const envelope = document.getElementById("envelope");
+const openBtn = document.getElementById("openBtn");
+const videoBox = document.getElementById("videoBox");
+const video = document.getElementById("video");
+const soundBtn = document.getElementById("soundBtn");
+
+openBtn.onclick = () => {
+    envelope.classList.add("open");
+
+    setTimeout(() => {
+        videoBox.style.display = "block";
+        video.play().catch(()=>{});
+    }, 1200);
+};
+
+soundBtn.onclick = () => {
+    video.muted = false;
+    video.volume = 1;
+    video.play();
+    soundBtn.innerText = "❤️ Enjoy the Video";
+};
+
+function createHeart(){
+    const heart=document.createElement("div");
+    heart.className="heart";
+    heart.innerHTML="❤️";
+    heart.style.left=Math.random()*100+"vw";
+    heart.style.animationDuration=(5+Math.random()*5)+"s";
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },10000);
+}
+
+setInterval(createHeart,500);
