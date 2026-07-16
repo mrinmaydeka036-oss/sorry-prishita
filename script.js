7f731b808e4655abf290e28a45ec17585ff80526
mrinmaydeka036-oss <mrinmaydeka036@gmail.com>
2026-07-16 11:25:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
-alert("JS Loaded");
-
 const envelope = document.getElementById("envelope");
 const openBtn = document.getElementById("openBtn");
 const videoBox = document.getElementById("videoBox");
 const video = document.getElementById("video");
 const soundBtn = document.getElementById("soundBtn");
 
-openBtn.onclick = () => {
+// Open letter
+openBtn.addEventListener("click", function () {
+
     envelope.classList.add("open");
 
-    setTimeout(() => {
-        videoBox.style.display = "block";
-        video.play().catch(() => {});
-    }, 1200);
-};
+    setTimeout(function () {
 
-soundBtn.onclick = () => {
+        videoBox.style.display = "block";
+
+        video.play().catch(() => {});
+
+        video.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    }, 1800);
+
+});
+
+// Turn on sound
+soundBtn.addEventListener("click", function () {
+
     video.muted = false;
     video.volume = 1;
-    video.play();
-    soundBtn.innerHTML = "❤️ Enjoy the Video";
-};
 
+    video.play();
+
+    soundBtn.innerHTML = "❤️ Enjoy The Video";
+
+});
+
+// Floating hearts
 function createHeart() {
+
     const heart = document.createElement("div");
-    heart.className = "heart";
+
+    heart.classList.add("heart");
+
     heart.innerHTML = "❤️";
+
     heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = (Math.random() * 5 + 5) + "s";
+
+    heart.style.animationDuration =
+        (Math.random() * 4 + 4) + "s";
 
     document.body.appendChild(heart);
 
     setTimeout(() => {
+
         heart.remove();
-    }, 10000);
+
+    }, 8000);
+
 }
 
 setInterval(createHeart, 500);
